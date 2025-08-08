d82d551fecdee504f3d1f067696b63be8f1bd164
artahadhahd <iamarta.azadegan@gmail.com>
2025-08-08 21:20:41 +0200

    basic reflex app

---
+++ rxconfig.py
@@ -0,0 +1,9 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="code_jam_jazzy_jacarandas_2025",
+    plugins=[
+        rx.plugins.SitemapPlugin(),
+        rx.plugins.TailwindV4Plugin(),
+    ],
+)
