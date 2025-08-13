73d13450e6d6c18f0c639155d0bb6aee75d5bc73
HEROgold <martijnwieringa28@gmail.com>
2025-08-13 22:34:30 +0200

    add header/navbar setup.

---
+++ code_jam_jazzy_jacarandas_2025/components/header.py
@@ -0,0 +1,10 @@
+import reflex as rx
+from code_jam_jazzy_jacarandas_2025.components.about import about_button
+
+
+def header() -> rx.Component:
+    """Render the header component."""
+    content = rx.vstack(
+        about_button()
+    )
+    return rx.center(content)
