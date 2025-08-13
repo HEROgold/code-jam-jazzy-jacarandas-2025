0c6b768c9a3a2c2398d98cc9a36b73f3946df7b2
HEROgold <martijnwieringa28@gmail.com>
2025-08-13 22:55:03 +0200

    pre-commit fixes

--- code_jam_jazzy_jacarandas_2025/components/about.py
+++ code_jam_jazzy_jacarandas_2025/components/about.py
@@ -5,6 +5,7 @@ def redirect_to_about() -> rx.event.EventSpec:
     """Redirect to the about page."""
     return rx.redirect("/about")
 
+
 def about_button() -> rx.Component:
     """Render the about button."""
     return rx.button("About", on_click=redirect_to_about)
--- code_jam_jazzy_jacarandas_2025/components/header.py
+++ code_jam_jazzy_jacarandas_2025/components/header.py
@@ -5,7 +5,5 @@ from code_jam_jazzy_jacarandas_2025.components.about import about_button
 
 def header() -> rx.Component:
     """Render the header component."""
-    content = rx.vstack(
-        about_button()
-    )
+    content = rx.vstack(about_button())
     return rx.center(content)
